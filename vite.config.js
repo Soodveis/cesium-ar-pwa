fe0a857a654bcd73e4a7c86a9adcc90ff75f05d7
Soodveis <zakonzarabotka.ru@gmail.com>
2025-04-09 23:48:49 +0300

    Исправлен base для GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,36 +2,27 @@ import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: './',
+  base: '/cesium-ar-pwa/',
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      includeAssets: ['favicon.ico'],
       manifest: {
-        name: 'Cesium AR App',
+        name: 'Cesium AR PWA',
         short_name: 'CesiumAR',
-        description: 'Augmented Reality with Cesium and WebXR',
-        theme_color: '#000000',
-        background_color: '#000000',
+        start_url: '/',
         display: 'standalone',
-        orientation: 'portrait',
-        start_url: '.',
+        background_color: '#000000',
+        theme_color: '#000000',
         icons: [
           {
-            src: 'pwa-icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
+            src: 'favicon.ico',
+            sizes: '64x64 32x32 24x24 16x16',
+            type: 'image/x-icon'
           }
         ]
-      },
-      workbox: {
-        maximumFileSizeToCacheInBytes: 6000000 // увеличим лимит на кеширование больших файлов
       }
     })
   ]
-})
+});
+
